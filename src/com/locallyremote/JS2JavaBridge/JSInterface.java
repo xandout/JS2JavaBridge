a52fa5d23df59e80cf1a148b461783b8275fdd59
Mitchell Turner <turnerdistributed@gmail.com>
2014-06-18 14:05:04 -0400

    Added GPS Feature TODO: Get current GPS Lock

--- src/com/locallyremote/JS2JavaBridge/JSInterface.java
+++ src/com/locallyremote/JS2JavaBridge/JSInterface.java
@@ -2,44 +2,66 @@ package com.locallyremote.JS2JavaBridge;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 /**
  * Created by Mitchell on 6/16/2014.
  */
 public class JSInterface {
+
     Context mContext;
-    JSInterface(Context c){
+
+
+    JSInterface(Context c) {
         mContext = c;
     }
 
-    public void showToast(String message){
+
+    public void showToast(String message) {
         Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
     }
 
 
-    public void SavePreferences(String key, String value){
+    public void SavePreferences(String key, String value) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(key, value);
         editor.commit();
     }
-    public String LoadPreferences(String key){
+
+    public String LoadPreferences(String key) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
         return sharedPreferences.getString(key, "");
     }
 
-    public void Vibrate(long milliseconds){
+    public void Vibrate(long milliseconds) {
         Vibrator v = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
         v.vibrate(milliseconds);
     }
 
-    public void SendSMS(String phoneNumber, String message){
+    public void SendSMS(String phoneNumber, String message) {
         SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumber,null, message,null,null);
+        sms.sendTextMessage(phoneNumber, null, message, null, null);
+    }
+
+    public String getLocation() {
+        Log.w("BroadPlex", "JSI.getLocation() called");
+        String loc;
+        GeoCoord geo = new GeoCoord(mContext);
+        Location mLocation = geo.getGPSLocation();
+
+        loc = Double.toString(mLocation.getLatitude()) + ", " + Double.toString(mLocation.getLongitude());
+        geo.stopUsingGPS();
+        return loc;
+
+
     }
 
 
